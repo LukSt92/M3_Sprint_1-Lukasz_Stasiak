4f39b545b3a6608f1644b759ba4a615a407c6357
LukSt92 <lukasz.m.stasiak@gmail.com>
2025-06-18 20:30:20 +0200

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-// listToDict
-// map
-// filter
-// reduce
-
-// Example for cars
 const cars = {
   modelS: { brand: "Tesla", color: "white", price: 79999 },
   corolla: { brand: "Toyota", color: "silver", price: 20000 },
@@ -12,8 +6,6 @@ const cars = {
   model3: { brand: "Tesla", color: "black", price: 39999 },
   beetle: { brand: "Volkswagen", color: "yellow", price: 18000 },
 };
-
-// Example for students
 const students = {
   alice: { age: 20, major: "Computer Science", gpa: 3.8 },
   bob: { age: 19, major: "Mathematics", gpa: 3.2 },
@@ -22,12 +14,10 @@ const students = {
   eric: { age: 20, major: "Psychology", gpa: 3.6 },
   fiona: { age: 19, major: "Literature", gpa: 3.4 },
 };
-
 interface Dict<T> {
   [k: string]: T;
 }
 
-// Array.prototype.map, but for Dict
 function mapDict<T>(
   dict: Dict<T>,
   callback: (value: T, key: string) => T
@@ -40,7 +30,6 @@ function mapDict<T>(
   return result;
 }
 
-// Array.prototype.filter, but for Dict
 function filterDict<T>(
   dict: Dict<T>,
   callback: (value: T, key: string) => boolean
@@ -53,7 +42,6 @@ function filterDict<T>(
   return result;
 }
 
-// Array.prototype.reduce, but for Dict
 function reduceDict<T, U>(
   dict: Dict<T>,
   callback: (acc: U, value: T, key: string) => U,
@@ -70,7 +58,6 @@ const mapTest = mapDict(students, (student) => ({
   ...student,
   gpa: student.gpa * 10,
 }));
-
 console.log(mapTest);
 
 const filterTest = filterDict(cars, (car) => car.price > 50000);
@@ -78,21 +65,17 @@ const filterTest2 = filterDict(
   cars,
   (car) => car.color === "black" || car.color === "silver"
 );
-
 console.log(filterTest);
 console.log(filterTest2);
 
 const reduceTest = reduceDict(cars, (acc, car) => acc + car.price, 0);
-
 console.log(reduceTest);
 
 const filterTest3 = filterDict(
   cars,
   (car) => car.price > 20000 && car.price < 50000
 );
-
 console.log(filterTest3);
 
 const reduceTest2 = reduceDict(filterTest3, (acc, car) => acc + car.price, 0);
-
 console.log(reduceTest2);
